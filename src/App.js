d757a982a3b9bdecb809bac29b60b1b2bb565186
Iyonissio Daniel Sitoe <57901563+Iyonissio@users.noreply.github.com>
2021-08-16 09:42:25 +0200

    Form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Form from './Form';
 
 function App() {
   return (
     <div className="App">
         <h1>Formulario</h1>
+
+        <Form />
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -1,1 +1,18 @@
 import React, { Component } from 'react';
+
+class Form extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <section>
+                Olaaa
+            </section>
+        )
+    }
+}
+
+export default Form;
