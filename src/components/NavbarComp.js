ecfaea09687f7c2326059461aa8ac3522194f5fe
Iyonissio Daniel Sitoe <57901563+Iyonissio@users.noreply.github.com>
2021-08-16 15:34:00 +0200

    Navbar

--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -1,35 +1,45 @@
 import React, { Component } from "react";
-import {Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';;
+import {
+  Navbar,
+  Nav,
+} from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
 
 export default class NavbarComp extends Component {
   render() {
     return (
-      <div>
-        <Navbar bg="dark" variant={"dark"} expand="lg">
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Router>
+        <div>
+          <Navbar bg="dark" variant={"dark"} expand="lg">
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
-                    Something
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
-                </NavDropdown>
+                <Nav.Link as={Link} to={"/home"}>Home</Nav.Link>
+                <Nav.Link as={Link} to={"/about"}>Sobre nos</Nav.Link>
+                <Nav.Link as={Link} to={"/contact"}>Contactos</Nav.Link>
               </Nav>
             </Navbar.Collapse>
-         
-        </Navbar>
-      </div>
+          </Navbar>
+        </div>
+        <div>
+            <Switch>
+                <Route path="/about">
+                    <About />
+                </Route>
+                <Route path="/contact">
+                    <Contact />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </div>
+      </Router>
     );
   }
 }
