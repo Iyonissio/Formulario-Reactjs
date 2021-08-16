c7776a1c86eaacfd92bd1a71dad0542684d7143b
Iyonissio Daniel Sitoe <57901563+Iyonissio@users.noreply.github.com>
2021-08-16 11:23:18 +0200

    Form 2 Finishe

--- src/Form2.js
+++ src/Form2.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
+import './App.css';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
-import { FacebookLoginButton } from 'react-social-login';
+import { FacebookLoginButton } from 'react-social-login-buttons';
 
 class Form2 extends Component {
     render() {
         return(
-            <Form className="App">
-                <h1>
+            <Form className="login-form">
+                <h1 className="text-center">
                     <span className="font-weight-bold">Meu Formulario</span>    
                 </h1>    
-                <h2>Bem vindo</h2>
+                <h2 className="text-center">Bem vindo</h2>
                 <FormGroup>
                     <Label>Email</Label>
                     <Input type="email" placeholder="Email"/>
                 </FormGroup>
+                <FormGroup className="pb-3">
+                    <Label>Password</Label>
+                    <Input type="password" placeholder="Password"/>
+                </FormGroup>
+                <div className="text-center btn-lg btn-dark btn-block pt-1">
+                    <Button className="btn-dark">Entrar</Button>
+                </div> 
+                <div className="text-center pt-3">
+                    Ou entre com a sua conta do Facebook
+                </div>
+                <FacebookLoginButton className="mt-3 mb-3"/>
+                <div className="text-center text-decoration-none">
+                    <a href="/sign-up">Cadastrar</a>
+                    <span className="p-2">|</span>
+                    <a href="/sign-up">Esqueceu a Password</a>
+                </div>
             </Form>  
         );      
     }
